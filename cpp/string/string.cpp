ad847c537818516590110e6f23ac7bffe4b46acd
IBISsaki <giraliusr@gmail.com>
2024-08-28 11:10:37 +0900

    Add files via upload

--- cpp/string/string.cpp
+++ cpp/string/string.cpp
@@ -2,8 +2,13 @@
 #include <cstring>
 #include "string.h"
 
-String::String();
-: str_ = (new char[1]), len_(0)
+std::ostream& operator << (std::ostream& out, const String& rhs)
+{
+    return out << rhs.str_;
+}
+
+String::String()
+: str_(new char[1]), len_(0)
 {
     assert(str_ );
     str_[0] = '\0';
@@ -28,4 +33,42 @@ String::String(const String& rhs)
     strcpy(str_, rhs.str_);
 }
 
-String::String& operator = (const String& rhs) {/* memberwise copy */ return *this }
+String& String::operator = (const String& rhs) 
+{
+    if(this != &rhs) {      // self assignment test! ex) s1 = s1; 자기 자신을 치환하려 할때 원본이 지워지는걸 막기 위함
+        delete [] str_; 
+        str_ = new char[rhs.len_ + 1];
+        assert(str_);
+        strcpy(str_, rhs.str_);
+        len_ = rhs.len_;
+    }
+    return *this;
+}
+
+bool String::operator == (const String& rhs) const
+{
+    return strcmp(str_, rhs.str_) == 0;
+}
+
+const String String::operator + (const String& rhs) const
+{
+    // char buf[len_ + rhs.len_ + 1];
+    char *buf = new char[len_ + rhs.len_ + 1];
+    strcpy(buf, str_);
+    strcat(buf, rhs.str_);
+
+    String result(buf);
+    delete [] buf;
+
+    return result;
+}
+
+const char *String::c_str() const
+{
+    return str_;
+}
+
+int String::length() const
+{
+    return len_;
+}
--- cpp/string/string.h
+++ cpp/string/string.h
@@ -1,7 +1,10 @@
 #ifndef STRING_H
 #define STRING_H
+#include <iostream>
 
 class String {
+friend std::ostream& operator << (std::ostream& out, const String& rhs);
+
 private:
     char *str_;
     int len_;
@@ -15,7 +18,15 @@ public:
     // String* operator&() { return this; }
     // const String* operator&() { return this; }
 
-    String& operator = (const String& rhs);
+    String& operator = (const String& rhs);     // 문자열 치환은 shallow copy 가 아닌  deep copy 방식을 써야한다.
+
+    bool operator == (const String& rhs) const;
+    // !=, >, <
+
+    const String operator + (const String& rhs) const;
+
+    const char *c_str() const;
+    int length() const;
 };
 
 #endif
