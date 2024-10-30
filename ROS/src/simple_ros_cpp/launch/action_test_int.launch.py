d6db3fb06765c7fa0f2ba9c43fc2754150fa9676
IBISsaki <giraliusr@gmail.com>
2024-10-30 16:32:20 +0900

    Delete ROS/src/simple_ros_cpp directory

--- ROS/src/simple_ros_cpp/launch/action_test_int.launch.py
+++
@@ -1,8 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package = "simple_ros_cpp", executable = "action_server"), 
-        Node(package = "simple_ros_cpp", executable = "action_client", arguments=['5'])
-    ])
