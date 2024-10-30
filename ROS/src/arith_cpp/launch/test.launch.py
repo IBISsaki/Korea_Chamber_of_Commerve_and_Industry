fdf840a0621333febddfa36e6c4016ef1f6be9b9
IBISsaki <giraliusr@gmail.com>
2024-10-30 16:32:39 +0900

    Delete ROS/src/arith_cpp directory

--- ROS/src/arith_cpp/launch/test.launch.py
+++
@@ -1,8 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package = "arith_cpp", executable = "argument"), 
-        Node(package = "arith_cpp", executable = "calculator")
-    ])
--- ROS/src/arith_cpp/src/calculator/calculator.cpp
+++
@@ -1,6 +0,0 @@
-// auto get_arithmetic_operator = [this](
-//     const shared_ptr<get_arithmetic_operator::Request> request,
-//     shared_ptr<get_arithmetic_operator::Response> response) -> void
-// {
-    
-// }
